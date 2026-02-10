961c2835a29d14ce382aceb239f9430487a778cc
harithaveeresh-qa <harithaveeresh@gmail.com>
2026-02-10 11:17:24 +0530

    Add About Us page, tests, and test data

---
+++ tests/test_aboutUs.py
@@ -0,0 +1,10 @@
+import pytest
+from pages.aboutUs_page import AbourUs
+from utils.csv_data_loader import CSVDataLoader
+
+@pytest.mark.smoke
+def test_AboutUs(page):
+    expected_titles = CSVDataLoader.load_expected_titles("testdata/custom_app.csv")
+
+    abpout_menu=AbourUs(page)
+    abpout_menu.click_about(expected_titles) 
